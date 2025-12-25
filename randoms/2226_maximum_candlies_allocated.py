0753241a1f2cd0526f8e0e663c6e8d7ac4d4edf9
romankarki <romnkrki@gmail.com>
2025-12-25 12:58:06 -0600

    max candies allocation problem solution

--- randoms/2226_maximum_candlies_allocated.py
+++ randoms/2226_maximum_candlies_allocated.py
@@ -1,4 +1,44 @@
 class Solution:
 
     def max_candies(self, candies, k):
-        pass
+        total = sum(candies)
+        if total < k: 
+            return 0 
+        if total == k: 
+            return 1
+        
+        l = 1
+        r = total // k
+
+        ans = l
+
+        while l <= r:
+            m = (l + r) // 2
+            if self.can_be_dist(candies, m, k) : 
+                ans = m 
+                l = m + 1
+            else:
+                r = m - 1 
+
+        return ans
+
+
+    def can_be_dist(self, candies, pile, target):
+
+        output = 0 
+        for each in candies: 
+
+            y = each // pile
+            output += y 
+
+        return output >= target
+
+
+i = [5, 8, 5, 100]
+k = 3
+
+s = Solution()
+
+a = s.max_candies(i, k)
+
+print("answer is",a)
