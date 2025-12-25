93d48d91b7c3cc2a70cd0e64f1202e05b4048562
romankarki <romnkrki@gmail.com>
2025-12-25 13:50:40 -0600

    maximize happiness - greedy slution

--- randoms/3075_maximize_happiness.py
+++ randoms/3075_maximize_happiness.py
@@ -4,4 +4,17 @@ class Solution:
 
     def maximum_happiness(self, hapinees: List[int], k : int) -> int:
 
-        pass
+        hapinees.sort()
+
+        picks = []
+
+        to_deduct = 0 
+        for _ in range(0, k):
+            curr = hapinees.pop()
+            pick = curr - to_deduct
+            if pick < 0 :
+                pick = 0 
+            picks.append(pick)
+            to_deduct += 1
+        return sum(picks)
+        
