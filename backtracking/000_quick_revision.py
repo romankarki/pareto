dcfe6420fbbfdf8027c547547f3b4ecf483386a0
romankarki <romnkrki@gmail.com>
2025-12-26 12:20:05 -0600

    backtracking permutation

--- backtracking/000_quick_revision.py
+++ backtracking/000_quick_revision.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
 
 
-    def permute_recursion(self, nums: List[int]) -> List[int]:
+    def permute_recursion(self, nums: List[int]) -> List[List[int]]:
         '''
         Docstring for permute_recursion
         
@@ -17,6 +17,8 @@ class Solution:
             - get and element from second to end of array 
             [1,2,3] -> [2,3] -> [3]
             -  
+        Time: O(n! * n^2)
+        Space: O(n! * n)
         '''
         if len(nums) == 0:
             return [[]]
@@ -37,7 +39,7 @@ class Solution:
         print("----------------------------------------") 
         return res
     
-    def permute_iterative(self, nums:List[int]) -> List[int]:
+    def permute_iterative(self, nums:List[int]) -> List[List[int]]:
         '''
         Docstring for permute_iterative
         
@@ -60,9 +62,27 @@ class Solution:
                     new_perms.append(p_copy)
             perms = new_perms
         return perms
+    
 
+    def permute_backtracking(self, nums:List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack([], nums, [False]* len(nums))
+        return self.res
+
+    def backtrack(self, perm: List[int], nums:List[int], pick: List[bool]) -> List[List[int]] :
+        if len(perm) == len(nums):
+            self.res.append(perm[:])
+            return 
+        for i in range(len(nums)):
+            if not pick[i]:
+                perm.append(nums[i])
+                pick[i] = True
+                self.backtrack(perm, nums, pick)
+                perm.pop()
+                pick[i] = False
 
 s = Solution()
 
 # print(s.permute_recursion([1,2,3]))
-print(s.permute_iterative([1,2,3]))
+# print(s.permute_iterative([1,2,3]))
+print(s.permute_backtracking([1,2,3]))
