28770df8b638e1f1f432ef609a15d36f07eb864b
romankarki <romnkrki@gmail.com>
2025-12-28 15:02:12 -0600

    unique path with dp and bottom up solution

--- dynamic_programming/2d/62_unique_path.py
+++ dynamic_programming/2d/62_unique_path.py
@@ -14,6 +14,17 @@ class Solution:
 
         dfs(0, 0)
         return self.ans
+    
+
+    def unique_dp(self, m, n):
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        dp[m-1][n-1] = 1
+
+
+        for r in range(m-1, -1, -1):
+            for c in range(n-1, -1, -1):
+                dp[r][c] += dp[r +1][c] + dp[r][c+1]
+        return dp[0][0]
 
 
 
@@ -22,4 +33,4 @@ m, n = 3, 7
 
 s = Solution()
 
-print("Possibel paths", s.unique_path(m, n))
+print("Possibel paths", s.unique_dp(m, n))
