9a50705196eef7989d01ff7bfe60507741bc0979
romankarki <romnkrki@gmail.com>
2025-12-29 16:33:24 -0600

    memoized solution for target sum

--- dynamic_programming/2d/494_target_sum.py
+++ dynamic_programming/2d/494_target_sum.py
@@ -12,8 +12,23 @@ class Solution:
 
         return backtrack(0, 0) 
         
+    
+    def target_sum_memo(self, nums: List[int], target: int) -> int:
+        
+        memo = {}
+        def backtrack(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            
+            if (i, total) in memo:
+                return memo[(i,total)]
+            
+            memo[(i, total)] = backtrack(i+1, total + nums[i]) + backtrack(i+1, total - nums[i])
+            return memo[(i, total)]
+        return backtrack(0,0)
 
 
 s = Solution()
 
-print(s.target_sums_brute([1,2,3,4,5], 5))
+# print(s.target_sums_brute([1,2,3,4,5], 5))
+print(s.target_sum_memo([1,2,3,4,5], 5))
