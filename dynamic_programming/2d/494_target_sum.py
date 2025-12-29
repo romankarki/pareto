1ddeefefda63ab7b866f729eca9209332a842ef3
romankarki <romnkrki@gmail.com>
2025-12-29 16:37:53 -0600

    iterative dp for target sum

--- dynamic_programming/2d/494_target_sum.py
+++ dynamic_programming/2d/494_target_sum.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 class Solution:
@@ -26,9 +27,23 @@ class Solution:
             memo[(i, total)] = backtrack(i+1, total + nums[i]) + backtrack(i+1, total - nums[i])
             return memo[(i, total)]
         return backtrack(0,0)
+    
+
+    def target_sum_iterative(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        dp = [defaultdict(int) for _ in range(n+1)]
+        dp[0][0] = 1
+
+        for i in range(n):
+            for total, count in dp[i].items():
+                dp[i+1][total + nums[i]] += count 
+                dp[i+1][total - nums[i]] += count
+        print(dp)
+        return dp[n][target]
 
 
 s = Solution()
 
 # print(s.target_sums_brute([1,2,3,4,5], 5))
-print(s.target_sum_memo([1,2,3,4,5], 5))
+# print(s.target_sum_memo([1,2,3,4,5], 5))
+print(s.target_sum_iterative([1,2,3,4,5], 5))
