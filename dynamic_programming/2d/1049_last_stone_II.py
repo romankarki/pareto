c01aaac056df740cafa1d3f3b38561c6c2a16a9e
romankarki <romnkrki@gmail.com>
2025-12-30 14:02:26 -0600

    last stone ii dp iterative done but have to revise

--- dynamic_programming/2d/1049_last_stone_II.py
+++ dynamic_programming/2d/1049_last_stone_II.py
@@ -15,3 +15,19 @@ class Solution:
             dp[(i,total)]  = min(dfs(i+1, total), dfs(i+1, total+stones[i]))
             return dp[(i, total)]
         return dfs(0,0) 
+    
+
+    def laststone_bottomup(self, stones: List[int]) -> int:
+        stone_sum = sum(stones)
+        target = stone_sum // 2
+        n = len(stones)
+
+        dp = [[0] * (target + 1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for t in range(target+1):
+                if t >= stones[i-1]:
+                    dp[i][t] = max(dp[i-1][t], dp[i-1][t-stones[i-1]]+ stones[1-1])
+                else:
+                    dp[i][t] = dp[i-1][t]
+        return stone_sum - 2 * dp[n][target]
