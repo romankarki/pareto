d1281698169eef0171d560575d98717980b26229
romankarki <romnkrki@gmail.com>
2026-01-01 18:04:33 -0600

    much more pythonic way for the same solutoin

--- daily_problems/66_plus_one.py
+++ daily_problems/66_plus_one.py
@@ -4,7 +4,6 @@ class Solution:
 
     def plus_one_personal_sol(self, digits: List[int]) -> List[int]:
         ans = [] 
-
         l, r = 0, len(digits) - 1
 
         digits[r] += 1
@@ -23,14 +22,24 @@ class Solution:
             
             r -= 1
         
-
-
         return ans + digits
+    
+
+    def plus_one_pythonic(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0 
+        
+        return [1] + digits
 
 
 
 
 s = Solution()
 
-op = s.plus_one_personal_sol([1,2,9,9])
+# op = s.plus_one_personal_sol([1,2,9,9])
+op = s.plus_one_pythonic([1,2,9,9,9])
 print(op)
