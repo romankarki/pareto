57a987bfd64bbc51d3e3bb6b652b29f43ff107f5
romankarki <romnkrki@gmail.com>
2026-01-04 13:28:41 -0600

    designing number container

---
+++ daily_problems/2349_design_number_container.py
@@ -0,0 +1,13 @@
+class Solution:
+
+
+    def __init__(self):
+        pass
+
+
+    def change(self, index: int, number: int) -> None :
+        pass
+
+
+    def find(self, number: int) -> int:
+        pass
