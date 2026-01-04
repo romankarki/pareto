a12f77e38f6d2ef1ea25b1413478b38040c9f2f7
romankarki <romnkrki@gmail.com>
2026-01-04 13:29:22 -0600

    design number contianer with heap and dict

--- daily_problems/2349_design_number_container.py
+++ daily_problems/2349_design_number_container.py
@@ -1,13 +1,25 @@
+from collections import defaultdict
+import heapq
+
 class Solution:
 
 
     def __init__(self):
-        pass
+        self.index_to_num = {}
+        self.num_to_index = defaultdict
 
 
     def change(self, index: int, number: int) -> None :
-        pass
+        self.index_to_num[index] = number
+        heapq.heappush(self.num_to_index[number], index)
 
 
     def find(self, number: int) -> int:
-        pass
+        if number not in self.num_to_index:
+            return -1
+        heap = self.num_to_index[number]
+        while heap:
+            if self.index_to_num.get(heap[0]) == number:
+                return heap[0]
+            heapq.heappop(heap)
+        return -1
