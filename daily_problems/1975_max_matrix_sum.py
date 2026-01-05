07aba89cfe276bac3873550e0ca10fa8a69b8c11
romankarki <romnkrki@gmail.com>
2026-01-05 08:54:03 -0600

    max matrix sum problem

---
+++ daily_problems/1975_max_matrix_sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+
+    def max_matrix_sum(self, matrix: List[List[int]]) -> int:
+
+        '''
+        - count negative
+        - even: abs_sum 
+        - odd: abs_sum - 2(minimum)
+        '''
+        count_neg = 0 
+        abs_sum = 0
+        mini = float('inf')
+        R, C = len(matrix), len(matrix[0])
+
+        for r  in range(0, R):
+            for c in range(0, C):
+                curr = matrix[r][c]
+                abs_sum += abs(curr)
+                mini = min(mini, abs(curr))
+                if curr < 0:
+                    count_neg += 1
+        
+        if count_neg % 2 == 1:
+            return abs_sum - (2*mini)
+        return abs_sum 
