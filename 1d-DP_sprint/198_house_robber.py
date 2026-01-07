fd464c4011e10d18e25e3f9a30f0c9af9c717ab9
romankarki <romnkrki@gmail.com>
2026-01-07 14:59:29 -0600

    brute force and memoization of house robber problem

--- 1d-DP_sprint/198_house_robber.py
+++ 1d-DP_sprint/198_house_robber.py
@@ -4,7 +4,10 @@ from typing import List
 class Solution: 
 
 
-    def hourse_robber_bruteforce (self, nums: List[int]) -> int : 
+    def house_robber_bruteforce (self, nums: List[int]) -> int : 
+        from functools import lru_cache
+
+        @lru_cache(None)
         def dfs(i: int) -> int: 
             if i >= len(nums):
                 return 0 
@@ -12,11 +15,45 @@ class Solution:
             return max(nums[i] + dfs(i+2), dfs(i+1))
         
         return dfs(0)
-    
 
+
+    def house_robber_memoization(self, nums: List[int]) -> int: 
+        memo = {}
+        def dfs(i: int) -> None:
+            if i >= len(nums): 
+                return 0 
+            if i in memo: 
+                return memo[i]
+
+            rob_curr = nums[i] + dfs(i+2)
+
+            skip_curr = dfs(i+1)
+
+            memo[i] = max(rob_curr, skip_curr)
+            return memo[i]
+        
+        return dfs(0)
+
+
+    def repractise_memo(self, nums: List[int]) -> int: 
+
+        def dfs(i: int) -> int:
+            if i >= len(nums): 
+                return 0 
+
+            take_curr = nums[i] + dfs(i+2)
+            skip_curr = dfs(i+1)
+
+            return max(take_curr, skip_curr) 
+
+
+        return dfs(0)
+    
 
 
 s = Solution()
 
 arr = [2,3,5,8,9,11, 19, 1, 12]
-print(s.hourse_robber_bruteforce(arr))
+print(s.house_robber_bruteforce(arr))
+print(s.house_robber_memoization(arr))
+print(s.repractise_memo(arr))
