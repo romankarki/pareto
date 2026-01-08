3401e51755a3ea74523fbdde342630f231c7a585
romankarki <romnkrki@gmail.com>
2026-01-07 19:11:18 -0600

    ugly number revision

--- 1d-DP_sprint/264_ugly_numbers.py
+++ 1d-DP_sprint/264_ugly_numbers.py
@@ -39,4 +39,25 @@ class Solution:
     
 
 
-    
+    def nth_ugly_num(self, n: int) -> int: 
+        dp = [1] * n 
+
+        i2 = i3 = i5 = 0 
+
+        for i in range(1, n): 
+            next2 = dp[i2] * 2
+            next3 = dp[i3] * 3
+            next5 = dp[i5] * 5
+
+            dp[i] = min(next2, next3, next5)
+
+            if dp[i] == next2: 
+                i2 += 1
+            if dp[i] == next3: 
+                i3 += 1
+            
+            if dp[i] == next5: 
+                i5 +=1
+
+
+        return dp[-1]
