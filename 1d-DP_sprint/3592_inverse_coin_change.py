bcc4c527302937865d3173e3d84a87f2514edb5b
romankarki <romnkrki@gmail.com>
2026-01-08 17:06:30 -0600

    inverse coin change

---
+++ 1d-DP_sprint/3592_inverse_coin_change.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution: 
+
+    def make_ways(self, coins: List[int], n: int) -> List[int]: 
+        dp  = [0] * (n+1)
+
+        dp[0] = 1
+        for c in coins: 
+            for j in range(c, n+1):
+                dp[j] += dp[j-c]
+
+        return dp[1:]
+
+    def brute_inverse(self, num_ways: int) -> List[int]: 
+
+        # TODO : LATER TOO MUCH DIFFICULT FOR TODAY
+        # n = len(num_ways)
+
+        # for r in range(1, n+1): 
+        #     for combo in combinations(range(1, n+1), r): 
+        #         if make_ways(combo)
+
