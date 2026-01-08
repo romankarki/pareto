2838a084762692b18236b607f174bfe52156d87f
romankarki <romnkrki@gmail.com>
2026-01-08 16:57:19 -0600

    max dot product

--- daily_problems/1458_max_dot_product.py
+++ daily_problems/1458_max_dot_product.py
@@ -15,7 +15,7 @@ class Solution:
         subs1 = all_subsequences(nums1)
         subs2 = all_subsequences(nums2)
 
-        ans = float('inf')
+        ans = float('-inf')
 
         for a in subs1: 
             for b in subs2: 
@@ -23,3 +23,47 @@ class Solution:
                     dot = sum(x * y for x, y in zip(a,b))
                     ans = max(ans, dot)
         return ans 
+
+
+    def max_dot_product_recursion(self, nums1: List[int], nums2: List[int]) -> int: 
+        m, n = len(nums1), len(nums2)
+
+        def dfs(i, j, taken): 
+            if i == m or j == n: 
+                return 0 if taken else float('-inf')
+            
+            #take both 
+            take = nums1[i] *  nums2[j] + dfs(i+1, j+1, True)
+
+            skip1 = dfs(i +1, j, taken)
+
+            skip2 = dfs(j, j+1, taken)
+
+            return max(take, skip1, skip2)
+            
+
+        return dfs(0, 0, False)
+
+
+    def max_dot_product_memoized(self, nums1: List[int], nums2: List[int]) -> int: 
+        m, n = len(nums1), len(nums2)
+
+        def dfs(i, j, taken): 
+
+            if i == m or i== n:
+                return 0 if taken else float('-inf')
+            
+            
+
+            return max(
+                nums1[i] * nums2[j] + dfs(i + 1, j + 1, True),
+                dfs(i + 1, j, taken), 
+                dfs(i +1, j+1, taken)
+                       )
+
+
+        return dfs(0, 0, False)
+
+
+
+
