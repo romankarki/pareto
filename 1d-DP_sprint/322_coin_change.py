3a3af48771f75584e28191196d45b2a5b29ab026
romankarki <romnkrki@gmail.com>
2026-01-08 12:53:49 -0600

    coinc change dp iterative approach

--- 1d-DP_sprint/322_coin_change.py
+++ 1d-DP_sprint/322_coin_change.py
@@ -57,8 +57,28 @@ class Solution:
         
         return -1 if min_coins == float('inf') else min_coins
 
+    def coin_change_dp(self, coins: List[int], amount: int) -> int:
+
+        '''
+        coins = [1, 3, 5] target = 11 
+
+        dp = [0 1 2 min(1 + dp(2), 1 + dp(0)) _ _ _ _ _ _ _ _]
+        '''
+        dp = [float('inf')] * (amount + 1)
+
+        dp[0] = 0 
+
+        for i in range(1, len(dp)): 
+            for c in coins: 
+                if amount >= c:
+                    dp[i] = min(dp[i], 1 + dp[i - c])
+        return dp[-1] if dp[-1] != float('inf') else -1
+
+
+
+
 
 
 s = Solution()
 
-print(s.coin_change_deduct([1,2,5], 11))
+print(s.coin_change_dp([1,2,5], 11))
