b5f113646732edf026ffca93bb5dd2353d8a4f16
romankarki <romnkrki@gmail.com>
2026-01-09 13:11:08 -0600

    re revising smallest subtree problem

--- daily_problems/865_smallest_subtree.py
+++ daily_problems/865_smallest_subtree.py
@@ -51,3 +51,27 @@ class Solution:
         
 
         return dfs(root)[1]
+
+
+
+    def subtree_smallest_deepest(self, root: TreeNode) -> TreeNode: 
+
+        def dfs(node: TreeNode) -> tuple[int, TreeNode]: 
+
+            if not node: 
+                return (0, None)
+            
+            left_depth, left_node = dfs(node.left)
+            right_depth, right_node = dfs(node.right)
+
+            if left_depth > right_depth: 
+                return (left_depth + 1, left_node)
+            
+            if right_depth > left_depth: 
+                return (right_depth +1, right_node)
+            
+
+            return (left_depth + 1, node)
+        
+
+        return dfs(root)[1]
