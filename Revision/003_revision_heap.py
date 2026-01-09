8e5db3f89a19b0e8623c939cee4bc9b8b11a1a41
romankarki <romnkrki@gmail.com>
2026-01-09 17:36:21 -0600

    median stream solut

--- Revision/003_revision_heap.py
+++ Revision/003_revision_heap.py
@@ -132,6 +132,27 @@ class Revision:
         return res
 
 
+class MedianStrem:
+
+
+    def __init__(self): 
+        self.small = [] # max
+        self.large = [] # min
+    
+
+    def add(self, num: int): 
+        heapq.heappush(self.small, -num)
+        heapq.heappush(self.large, - heapq.heappop(self.small))
+
+        if len(self.large) > len(self.small): 
+            heapq.heappush(self.small, -heapq.heappop(self.large))
+    
+    def median(self):
+        if len(self.small) > len(self.large): 
+            return -self.small[0]
+        return (-self.small[0] + self.large[0]) / 2
+
+
 
     
 
