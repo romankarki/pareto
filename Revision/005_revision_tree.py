cd20eb3b1a3a2857edc3fe146d7eae18cbbab4cc
romankarki <romnkrki@gmail.com>
2026-01-10 14:56:23 -0600

    bfs and level order traversing practise

--- Revision/005_revision_tree.py
+++ Revision/005_revision_tree.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from collections import deque
 
 class TreeNode: 
     def __init__(self, val = 0, left = None, right = None): 
@@ -20,3 +20,23 @@ class Revision:
         #in
         self.pattern1_dfs_1(root.right)
         #post
+    
+
+    def pattern2_bfs_1(self, root: Optional[TreeNode]) -> None:
+
+        if not root: 
+            return [] 
+        
+        q = deque([root])
+        res = []
+        while q: 
+            level = [] 
+            for _ in range(len(q)): 
+                node = q.popleft()
+                level.append(node.val)
+                if node.left: 
+                    q.append(node.left)
+                if node.right: 
+                    q.append(node.right)
+            res.append(level)
+        return res
