5669b7c7cfb2aeb976dc88a41d76d15e4fe031ab
romankarki <romnkrki@gmail.com>
2026-01-10 15:33:53 -0600

    binary tree path sum problem

--- Revision/005_revision_tree.py
+++ Revision/005_revision_tree.py
@@ -155,3 +155,18 @@ class Revision:
 
         dfs(root, target, [])
         return self.ans
+    
+    def max_path_sum_btree(self, root): 
+        self.maxSum = float('-inf')
+
+        def dfs(node): 
+            if not node: 
+                return 0
+            left = max(0, dfs(node.left))
+            right = max(0, dfs(node.right))
+            self.maxSum = max(self.maxSum,node.val + left + right)
+            return node.val + max(left, right)
+            
+
+        dfs(root)
+        return self.maxSum
