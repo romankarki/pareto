a4958e7940e08385469896c51158176241ffdd63
romankarki <romnkrki@gmail.com>
2026-01-10 13:05:35 -0600

    min ascii delete probelm done

---
+++ daily_problems/712_min_asci_delete.py
@@ -0,0 +1,23 @@
+class Solution: 
+
+
+    def min_ascii_diff(self, s1: str, s2: str) -> int: 
+        
+        def dfs(i: int, j: int) -> int: 
+
+            if i == len(s1):
+                return sum([ord(c) for c in s2[j:]])
+
+            if j == len(s2): 
+                return sum([ord(c) for c in s1[i:]])
+
+            if s1[i]  == s2[j]: 
+                dfs(i+1, j+1)
+
+
+            return min(
+                ord(s1[i]) + dfs(i+1, j),
+                ord(s2[j]) + dfs(i, j+1)
+                )
+        
+        return dfs(0, 0)
