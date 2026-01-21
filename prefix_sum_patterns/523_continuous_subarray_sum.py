f674f8a0b35c2cd6baf6dbc675847ce7de35ea1f
romankarki <romnkrki@gmail.com>
2026-01-21 13:21:42 -0600

    brute forcing continuous subarray sum

---
+++ prefix_sum_patterns/523_continuous_subarray_sum.py
@@ -0,0 +1,47 @@
+from typing import List 
+
+
+class Solution: 
+
+
+    def check_subarray_sum(self, nums: List[int], k: int) -> bool: 
+        '''
+        input = [23, 2, 4, 6, 7] and k = 6 
+        output = true
+        exp: [2,4] is sum 6 and len >= 2 and is multiple of k 
+
+
+        input = [23, 2, 6, 4, 7] and k = 6 
+        output = true
+        exp: total sum is 42 of len >= 2 and is multiple of k = 6 
+
+        brute force will be o(n**2)
+        '''
+        
+        pass
+
+
+
+    def brute_force(self, nums: List[int], k: int) -> bool: 
+        #o(n**2) 
+        for i in range(0, len(nums) - 1): 
+            total = 0 + nums[i]
+            for j in range(i+ 1, len(nums)): 
+                total += nums[j]
+                if total % k == 0: 
+                    return True 
+        
+        return False 
+
+
+
+
+
+s = Solution()
+
+print(s.brute_force([23, 2, 6, 4, 7, 13], 13))
+print(s.brute_force([23, 2, 6, 4, 7], 6))
+
+
+
+
