4ec5b252467ccaf1b1b3248e41af0817c626f484
romankarki <romnkrki@gmail.com>
2026-01-25 14:19:28 -0600

    daily problem for jan 25

---
+++ daily_problems/1984_min_diff_high_low.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+
+
+    def mindiff(self, nums: List[int], k: int) -> int: 
+
+
+        if len(nums) < k :
+            return 0 
+        
+
+        nums.sort()
+
+        l, r = 0, k-1
+
+        ans = float("inf")
+
+        while r < len(nums):
+            diff = nums[r] - nums[l]
+            ans = min(diff, ans)
+
+            r += 1
+            l += 1
+        return ans 
