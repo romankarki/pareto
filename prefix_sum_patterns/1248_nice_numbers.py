b288c1e24d0f1294b3c66dad6e2c480b30005fe3
romankarki <romnkrki@gmail.com>
2026-01-26 17:44:06 -0600

    self explanable solution

--- prefix_sum_patterns/1248_nice_numbers.py
+++ prefix_sum_patterns/1248_nice_numbers.py
@@ -1,5 +1,5 @@
 from typing import List 
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 class Solution: 
@@ -16,11 +16,29 @@ class Solution:
             res += mp[prefix - k]
             mp[prefix] += 1
         
-
         print(mp)
 
-        
         return res 
+    
+
+    def nice_numbers_subarray(self, nums: List[int], k: int):
+
+        '''
+        
+        '''
+
+
+        prefix_count= Counter({0: 1})
+        odd_sum = 0 
+        result = 0 
+
+        for num in nums: 
+            odd_sum += num % 2 
+            result += prefix_count[odd_sum - k]
+            prefix_count[odd_sum] += 1
+
+        return result 
+
     
 
 s = Solution()
