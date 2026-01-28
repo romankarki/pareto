ae51defb5048ee154f09aa7c459fa5c857a604a0
romankarki <romnkrki@gmail.com>
2026-01-28 10:52:40 -0600

    todo added

---
+++ daily_problems/3651_min_cos_path_teleportation.py
@@ -0,0 +1,18 @@
+from typing import List 
+
+
+class Solution: 
+
+
+    def min_cost(self, grid: List[List[int]], k: int) -> int: 
+        '''
+         grid = [
+            [1,3,3], 
+            [2,5,4], 
+            [4,3,5]
+         ]
+        '''
+
+
+
+        pass
