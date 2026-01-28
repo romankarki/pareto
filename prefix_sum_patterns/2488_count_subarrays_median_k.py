2d3e6af7b128d3f5da25865fd6a1257d5b2d4ffd
romankarki <romnkrki@gmail.com>
2026-01-28 10:31:12 -0600

    dry runing count subarrays median

--- prefix_sum_patterns/2488_count_subarrays_median_k.py
+++ prefix_sum_patterns/2488_count_subarrays_median_k.py
@@ -19,12 +19,38 @@ class Solution:
           i = 2 -> 1 -> balance = -1 -> right_count[1] + right_count[2] = 1 + 0 
         '''
 
-        #one way is brute force like o(n**2) and calling o(n logn) for sorting and median 
+        #one way is brute force like o(n**2) and calling o(n logn) for sorting and median
+        # 
+        '''
+        Lets do dryrun 
+
+        [3,2,1,4,5,6] and k = 4
+
+        pos = 3
+        right_count = { 
+            0: 1, 
+            1: 1, 
+            2: 1
+        }
+        res = count{[4], [4,5], [1,4,5], [2,1,4,5,6], [1,4,5,6]}
+
+
+        let's do for 
+        [3,2,1,5,6,7,4] and k = 5
+
+        pos = 3
+        right_count = { 
+            0: 1, 1: 2, 2: 1
+        }
+
+        res = count{[5], [1,5,6],[1,5,6,7], [2,1,5,6,7]}
+
+        ''' 
 
         pos = nums.index(k)
 
         right_count = Counter()
-        balance = 0 
+        balance = 0
         right_count[0] = 1
 
         for i in range(pos + 1, len(nums)):
@@ -36,11 +62,20 @@ class Solution:
         
         res = 0 
         balance = 0 
+
         for i in range(pos, -1, -1):
             if nums[i] > k: 
                 balance += 1
-            elif nums[i] < k:
+            elif nums[i] < k: 
                 balance -= 1
             res += right_count[-balance]
             res += right_count[-balance + 1]
-        return res 
+
+        return res      
+
+
+
+s = Solution()
+
+print(s.count_subarrays([3,2,1,5,6,7,4] , 5))
+print(s.count_subarrays([3,2,1,4,5,6], 4))
