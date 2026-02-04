a48d277ee3edcaa80d59518826e5e862b5bf1d1e
romankarki <romnkrki@gmail.com>
2026-02-04 16:11:08 -0600

    trionic array II rectified solution

--- daily_problems/3640_trionic_array_II.py
+++ daily_problems/3640_trionic_array_II.py
@@ -6,34 +6,59 @@ class Solution:
 
     def trionic_ii_sum(self, nums: List[int]) -> int: 
         n = len(nums)
+        ans = float("-inf")
+        i = 0
 
-        '''
-        f[i][state] = max sum ending at i in 'state'
-        (0: inc, 1: inc-dec, 2: inc-dec-inc)
-        init with -infinity 
-        '''
-        f = [[-float('inf')] * 3 for _ in range(n)] 
+        while i < n:
+            j = i + 1
+            res = 0
 
-        for i in range(n):
-            f[i][0] = nums[i]
-            if i > 0 and nums[i] > nums[i-1]: 
-                f[i][0] = max(f[i][0], f[i-1][0] + nums[i])
-            
-            if i > 0 and nums[i] < nums[i-1]:
-                if f[i-1][0] != -float('inf'):
-                    f[i][1] = max(f[i][1], f[i-1][0] + nums[i])
-                
-                if f[i-1][1] != -float('inf'):
-                    f[i][1] = max(f[i][1], f[i-1][1] + nums[i])
-            
-            if i > 0 and nums[i] > nums[i-1]:
-                if f[i-1][1] != -float('inf'):
-                    f[i][2] = max(f[i][2], f[i-1][1] + nums[i])
-                if f[i-1][2] != -float('inf'):
-                    f[i][2] = max(f[i][2], f[i-1][2] + nums[i])
+            # first segment: increasing segment
+            while j < n and nums[j - 1] < nums[j]:
+                j += 1
+            p = j - 1
 
-        ans = max(row[2] for row in f)
-        return ans 
+            if p == i: #increasing segment
+                i += 1
+                continue
+
+            # second segment: decreasing segment
+            res += nums[p] + nums[p - 1]
+            while j < n and nums[j - 1] > nums[j]:
+                res += nums[j]
+                j += 1
+            q = j - 1
+
+            if q == p or q == n - 1 or (j < n and nums[j] <= nums[q]):
+                i = q
+                continue
+
+            # third segment: increasing segment
+            res += nums[q + 1]
+
+            # find the maximum sum of the third segment
+            max_sum = 0
+            curr_sum = 0
+            k = q + 2
+            while k < n and nums[k] > nums[k - 1]:
+                curr_sum += nums[k]
+                max_sum = max(max_sum, curr_sum)
+                k += 1
+            res += max_sum
+
+            # find the maximum sum of the first segment
+            max_sum = 0
+            curr_sum = 0
+            for k in range(p - 2, i - 1, -1):
+                curr_sum += nums[k]
+                max_sum = max(max_sum, curr_sum)
+            res += max_sum
+
+            # update answer
+            ans = max(ans, res)
+            i = q
+
+        return ans
     
 
 
