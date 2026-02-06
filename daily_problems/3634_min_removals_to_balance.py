7ad23010ce78ad9b9fcb0381cc9012c48fe86f0e
romankarki <romnkrki@gmail.com>
2026-02-06 17:01:17 -0600

    min removals to balacne

---
+++ daily_problems/3634_min_removals_to_balance.py
@@ -0,0 +1,22 @@
+from typing import List 
+
+
+class Solution: 
+
+
+    def min_removals(self, nums: List[int], k: int) -> int: 
+
+
+        nums.sort()
+
+        r = 0 
+        n = len(nums)
+        ans = n 
+
+        for l in range(n):
+
+            while r < n and nums[r] <= (k * nums[l]):
+                r += 1
+            
+            ans = min(ans, n - (r - l) )
+        return ans 
