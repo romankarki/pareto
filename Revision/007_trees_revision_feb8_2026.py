b8743b8607aa690866239afe978dbc2415333a9f
romankarki <romnkrki@gmail.com>
2026-02-08 02:23:35 -0600

    revision of level order traversal

--- Revision/007_trees_revision_feb8_2026.py
+++ Revision/007_trees_revision_feb8_2026.py
@@ -26,3 +26,27 @@ def traversal(root: Optional[TreeNode]) -> List[int]:
 
 
 
+
+def level_order(root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+        return []
+    
+    result = []
+    q = deque([root])
+
+    while  q: 
+        level_size = len(q)
+        current_level = [] 
+
+        for _ in range(level_size):
+            node = q.popleft()
+            current_level.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        
+        result.append(current_level)
+
+
+    return result
