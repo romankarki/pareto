44c8ef7a4012d14f2760fbb7bc3ef9fc1f7e7867
romankarki <romnkrki@gmail.com>
2026-02-08 02:48:19 -0600

    right side viewg

--- Revision/007_trees_revision_feb8_2026.py
+++ Revision/007_trees_revision_feb8_2026.py
@@ -50,3 +50,23 @@ def level_order(root: Optional[TreeNode]) -> List[List[int]]:
 
 
     return result
+
+def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    if not root:
+        return []
+    result = []
+    q = deque([root])
+    
+    while q:
+        level_size= len(q)
+        current_level = []
+        for _ in range(level_size):
+            node = q.popleft()
+            current_level.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        last_item = current_level[-1]
+        result.append(last_item)
+    return result 
