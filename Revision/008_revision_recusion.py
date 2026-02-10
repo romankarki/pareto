05a6f9102589625e77fa43cfefa29aa9188d298f
romankarki <romnkrki@gmail.com>
2026-02-10 15:23:58 -0600

    space optimized dp fib

--- Revision/008_revision_recusion.py
+++ Revision/008_revision_recusion.py
@@ -43,3 +43,15 @@ class Solution:
 
         return dp[n]
     
+    def fib_space_optimized(self, n):
+        if n <= 1:
+            return n 
+        
+        prev2, prev1 = 0, 1
+
+        for i in range(2, n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return prev1
+    
