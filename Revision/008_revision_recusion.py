00c938ea631ce59bb3c3870d341838188b5677cd
romankarki <romnkrki@gmail.com>
2026-02-10 15:22:38 -0600

    basic dp with calculation of table

--- Revision/008_revision_recusion.py
+++ Revision/008_revision_recusion.py
@@ -31,3 +31,15 @@ class Solution:
         
         return self.fib_simple(n-1) + self.fib_simple(n-2)
     
+
+    def fib_tabulation(self, n):
+        if n <= 1:
+            return 1
+        
+        dp = [0] * (n+1)
+        dp[1]
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+    
