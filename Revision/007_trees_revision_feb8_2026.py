c7516a3592ed768d6665178383963a14c63a9ec4
romankarki <romnkrki@gmail.com>
2026-02-11 10:41:32 -0600

    kth smallest element recursive

--- Revision/007_trees_revision_feb8_2026.py
+++ Revision/007_trees_revision_feb8_2026.py
@@ -376,6 +376,30 @@ class Codec:
 
         return root 
 
+
+class KSmallest_Tree:
+
+
+    def kth_smallest(self, root: Optional[TreeNode], k: int) -> int: 
+        # let's use recursion for readability
+
+        self.k = k 
+        self.ans = None 
+
+        def inorder(node):
+            if not node or self.ans is not None:
+                return 
+          
+            inorder(node.left)
+            self.k -=1 
+            if self.k == 0:
+                self.ans = node.val 
+                return 
+            inorder(node.right)
+
+        inorder(root)
+        return self.ans 
+
     
 
 
