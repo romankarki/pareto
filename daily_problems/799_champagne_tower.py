11d3f9941b668d3e0931fb52dff870d61e7f0130
romankarki <romnkrki@gmail.com>
2026-02-14 22:04:29 -0600

    champagne_tower initial wrong assumption noted

---
+++ daily_problems/799_champagne_tower.py
@@ -0,0 +1,12 @@
+class Solution:
+
+
+
+    def champagne_tower(self, poured: int, query_row: int, query_glass: int) -> float:
+        '''
+        champagne doesn't flow level by level - keep that in mind -> was wrong initially on that 
+        '''
+        
+    
+
+        return 0.00
