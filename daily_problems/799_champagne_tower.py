5c199fcdd477af63133f12a765c66adfbf0d8a30
romankarki <romnkrki@gmail.com>
2026-02-15 16:46:53 -0600

    overflow simulation in champagne done

--- daily_problems/799_champagne_tower.py
+++ daily_problems/799_champagne_tower.py
@@ -6,7 +6,17 @@ class Solution:
         '''
         champagne doesn't flow level by level - keep that in mind -> was wrong initially on that 
         '''
-        
-    
 
-        return 0.00
+        tower = [[0.0 for _ in range(i+1)] for i in range(query_row + 1)]
+        tower[0][0] = poured
+
+        for row in range(query_row):
+            for glass in range(row+1):
+                overflow = (tower[row][glass] - 1.0)/2.0
+                if overflow > 0:
+                    tower[row+1][glass] += overflow
+                    tower[row+1][glass+1] += overflow
+        
+
+
+        return min(1.0, tower[query_row][query_glass])
