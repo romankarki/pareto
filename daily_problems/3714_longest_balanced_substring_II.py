387c7efcc13eaabd929798aa0a28fc5896381973
romankarki <romnkrki@gmail.com>
2026-02-15 17:33:21 -0600

    case by case coding for longest subsgtring solution

--- daily_problems/3714_longest_balanced_substring_II.py
+++ daily_problems/3714_longest_balanced_substring_II.py
@@ -2,28 +2,54 @@ class Solution:
 
 
     def longest_balanced(self, s: str) -> int:
-        state_map = {tuple(): -1}
-        count = {}
+        n = len(s)
+        ans = 1
 
-        max_length = 0 
-
-        for i, char in enumerate(s):
-            count[char] = count.get(char, 0) + 1
-
-            chars = sorted(count.keys())
-            if len(chars) == 1:
-                max_length = max(max_length, 1)
-                continue
-
-            first_char = chars[0]
-            state = tuple(count[c] - count[first_char] for c in chars[1:])
-
-            if state in state_map:
-                length = i - state_map[state]
-                max_length = max(max_length, length)
+        # case 1 - single char runs
+        cnt = 1 
+        for i in range(1, n):
+            if s[i] == s[i-1]:
+                cnt += 1
+                ans = max(ans, cnt)
             else:
-                state_map[state] = i
-            
+                cnt = 1
         
+        #case 2 - exactly two chars
+        pairs = [('a','b'), ('a','c'), ('b','c')]
 
-        return max_length
+        for x, y in pairs:
+            diff_map = {0: -1}
+            dx = dy = 0 
+
+            for i, ch in enumerate(s):
+                if ch == x:
+                    dx += 1
+                elif ch == y:
+                    dy += 1
+                else:
+                    diff_map = {0: i}
+                    dx = dy = 0 
+                    continue 
+                diff = dx - dy 
+                if diff in diff_map:
+                    ans = max(ans, i - diff_map[diff])
+                else:
+                    diff_map[diff] = i 
+        # case 3: all three chars
+
+        ca = cb = cc = 0 
+        diff_map = {(0,0):-1}
+
+        for i, ch in enumerate(s):
+            if ch =='a':
+                ca += 1
+            elif ch =='b':
+                cb += 1
+            else:
+                cc += 1
+            key = (ca - cb, ca - cc)
+            if key in diff_map:
+                ans = max(ans, i- diff_map[key])
+            else:
+                diff_map[key] = i 
+        return ans 
