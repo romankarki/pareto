940e18a433d58ad1854e82aa38e1addfa6a31707
romankarki <romnkrki@gmail.com>
2026-03-02 19:19:33 -0600

    solution for kthbit of nth binary

--- daily_problems/1545_kth_bit_nth_binary.py
+++ daily_problems/1545_kth_bit_nth_binary.py
@@ -2,5 +2,20 @@ class Solution:
 
 
     def find_kth_bit(self, n: int, k: int):
-        pass
+        s = "0"
+        for i in range(1,n):
+            new_s = self.invert_reverse(s)
+            s = s + "1" + new_s
+        return s[k-1]
     
+
+
+    def invert_reverse(self, s):
+        n_s = ""
+
+        for each in s:
+            if each == "0":
+                n_s += "1"
+            else:
+                n_s += "0"
+        return n_s[::-1]
