3fad7f6066653980a22472c4eea17edb803d6ecd
romankarki <romnkrki@gmail.com>
2026-03-02 18:06:35 -0600

    binary grid

---
+++ daily_problems/1536_min_swaps_binary_grid.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+     
+
+    def min_swaps(self, grid: List[List[int]]) -> int: 
+        n = len(grid)
+        trailing = []
+
+        for row in grid:
+            count = 0 
+            for j in range(n-1, -1, -1):
+                if row[j] == 0:
+                    count += 1
+                else:
+                    break
+            trailing.append(count)
+
+        swaps = 0 
+
+        for i in range(n):
+            need = n - 1 - i
+            found = -1
+
+            for j in range(i, n):
+                if trailing[j] >= need:
+                    found = j 
+                    break
+            if found == -1:
+                return -1
+            
+            while found > i:
+                trailing[found], trailing[found - 1] = trailing[found-1], trailing[found]
+                found += 1
+            
+        
+        return swaps 
