2513631a61808b204a7a1f356a0ce09ccffe2a76
romankarki <romnkrki@gmail.com>
2026-03-02 19:33:29 -0600

    cnacatenation of consecutinve binary numbers

---
+++ daily_problems/1680_concatenation_consecutive_binary.py
@@ -0,0 +1,16 @@
+class Solution:
+
+
+    def concatenation(self, n: int) -> int:
+
+        s = ""
+
+        for i in range(1, n+1):
+
+            b = str(bin(i))
+            s += b[2:]
+        
+        if s == "": return 0
+
+        modulo = 10**9 + 7
+        return int(s, 2) % modulo
