64643dd214f1f4ee5e7c4a6a0ed2157d14f79f79
romankarki <romnkrki@gmail.com>
2026-03-14 18:02:30 -0500

    increasing decreasing string akuna capital

---
+++ Company/Akuna Capital/1370_increasing_decreasing_string.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+class Solution:
+
+
+    def increasing_decreasing_sort(self, s: str) -> str: 
+
+        c = Counter(s)
+        ans = ''
+        alphabets  =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+        iteration = 1 
+
+        while len(ans) < len(s):
+
+            build = ''
+            data  = alphabets if iteration % 2 == 1 else reversed(alphabets)
+
+            for each in data:
+                if each in c and c[each] > 0:
+                    build += each 
+                    c[each] -= 1
+            ans += build 
+            iteration += 1
+        return ans
