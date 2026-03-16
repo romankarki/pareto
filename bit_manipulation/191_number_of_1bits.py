42d5463be3d9066a6c3cc08602d8329de9563cad
romankarki <romnkrki@gmail.com>
2026-03-15 23:31:12 -0500

    number of 1 bits simple but o(32) method

--- bit_manipulation/191_number_of_1bits.py
+++ bit_manipulation/191_number_of_1bits.py
@@ -10,3 +10,10 @@ class Solution:
             n = n & (n-1)
             count += 1
         return count 
+    
+    def hamming_shift(self, n):
+        res = 0 
+        while n > 0:
+            res += n % 2
+            n = n >> 1
+        return res
