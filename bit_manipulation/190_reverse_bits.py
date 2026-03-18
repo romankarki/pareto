328ebeb045296008aa3fe83cd4c36d290836c0fb
romankarki <romnkrki@gmail.com>
2026-03-17 22:14:35 -0500

    oring instead of adding

--- bit_manipulation/190_reverse_bits.py
+++ bit_manipulation/190_reverse_bits.py
@@ -4,6 +4,6 @@ class Solution:
         res = 0 
         for i in range(32):
             bit = (n >> i) & 1
-            res += (bit << (31-i))
+            res |= (bit << (31-i))
 
         return res 
