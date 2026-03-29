559bbe991634df5aa01f376d8f5d4e4671d9e706
romankarki <romnkrki@gmail.com>
2026-03-29 13:11:18 -0500

    desc for subarray sum odd

--- prefix_sum_patterns/1525_num_of_sub_array_with_odd_sum.py
+++ prefix_sum_patterns/1525_num_of_sub_array_with_odd_sum.py
@@ -2,7 +2,15 @@ from typing import List
 
 
 class Solution: 
-    def num_of_subarray(self, arr: List[int]) -> int: 
+    def num_of_subarray(self, arr: List[int]) -> int:
+        '''
+        I scan the array and see check each array is even or odd and respectively add even_count += 1 or odd_count +=1 if even or odd 
+
+        if even then all the odd possibilites with this even also makes odd so result += odd_count 
+        if odd  then all the even possibilities with this odd makes odd so result += even_count
+
+        and even_count start with 1 as 0 is even
+        ''' 
         MOD = 10**9 + 7 
         running_sum = 0 
         odd_count = 0 
