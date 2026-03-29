beeaf3bcf91a7456b988cfad9e6c62dcb06f651e
romankarki <romnkrki@gmail.com>
2026-03-29 12:21:23 -0500

    contigous array

--- prefix_sum_patterns/525_contiguous_array.py
+++ prefix_sum_patterns/525_contiguous_array.py
@@ -4,6 +4,13 @@ class Solution:
 
 
     def contigous_array(self, nums: List[int]) -> int: 
+        '''
+        Basic concept:
+        1. Running sum +1 for 1 and -1 for 0 
+        2. if running_sum seen in prefix_map -> found the diff between i - prefix at that
+        in that case equal 0 and 1 is there 
+        3. track the max lenght for this behaviour  
+        '''
         prefix_map = {0: -1}
 
         max_len = 0 
