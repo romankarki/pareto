bca3c636aee40e58db25e6281d65c92e5729a7ae
romankarki <romnkrki@gmail.com>
2026-04-02 18:27:32 -0500

    longest susbtring without repeating char

--- Company/Tesla/3_longest_substring_without_repeating_char.py
+++ Company/Tesla/3_longest_substring_without_repeating_char.py
@@ -16,3 +16,21 @@ class Solution:
                 seen.remove(s[l])
                 l += 1 
         return result 
+    
+
+    def longest_substring_revision1(self, s: str) -> int: 
+        longest = 0 
+        l, r = 0, 0 
+        seen = set()
+
+        while r < len(s):
+            if s[r] not in seen:
+                seen.add(s[r])
+                r += 1
+                longest = max(longest,len(seen) )
+            else:
+                seen.remove(s[l])
+                l += 1 
+
+
+        return longest 
