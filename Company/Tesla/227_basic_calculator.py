004a23c98c4f24de4710b5eb7fb923a19f76b76b
romankarki <romnkrki@gmail.com>
2026-04-02 12:20:03 -0500

    basic calculator operation using stack

---
+++ Company/Tesla/227_basic_calculator.py
@@ -0,0 +1,25 @@
+class Solution: 
+
+
+    def basic_calculator_ii(self, s: str) -> int: 
+        stack = []
+        num = 0 
+        prev_op = '+'
+
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            
+            if ch in '+-*/' or i == len(s) - 1:
+                if prev_op == '+':
+                    stack.append(num)
+                elif prev_op == '-':
+                    stack.append(-num)
+                elif prev_op == '*':
+                    stack.append(stack.pop() * num)
+                elif prev_op == '/':
+                    stack.append(int(stack.pop() / num))
+                prev_op = ch 
+                num = 0 
+        
+        return sum(stack)
