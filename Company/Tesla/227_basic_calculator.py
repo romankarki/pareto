ffe5339c35d3c7c94d95d93af63c0b6e083eba51
romankarki <romnkrki@gmail.com>
2026-04-02 12:34:38 -0500

    basic calculator revision 2

--- Company/Tesla/227_basic_calculator.py
+++ Company/Tesla/227_basic_calculator.py
@@ -33,7 +33,7 @@ class Solution:
             if ch.isdigit():
                 num = num * 10 + int(ch)
             
-            if ch in '+-*/':
+            if ch in '+-*/' or i == len(s) - 1:
 
                 if prev_op == "+":
                     stack.append(num)
@@ -46,5 +46,29 @@ class Solution:
                 prev_op = ch 
                 num = 0 
 
+        return sum(stack)
+
+
+
+    def basic_calculator_revision_2(self, s: str) -> int:
+        stack = []
+        num = 0 
+        last_op  =  "+"
+
+        for i, ch in s: 
+            if ch.isdigit(): 
+                num = num * 10 + int(ch)
+            
+            if ch in '+-/*' or i == len(s) -1:
+                if last_op == '+':
+                    stack.append(num)
+                elif last_op == "-":
+                    stack.append(-num)
+                elif last_op == "*":
+                    stack.append(stack.pop() * num )
+                elif last_op == "/":
+                    stack.append(int(stack.pop() / num) )
+                last_op = ch 
+                num = 0
         return sum(stack)
         
