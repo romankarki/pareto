a5a647dfe9c2ff6be5f4b9b6fd9a6d43ac985b20
romankarki <romnkrki@gmail.com>
2026-04-02 17:46:36 -0500

    three number sum revision 1

--- Company/Tesla/0015_3sum.py
+++ Company/Tesla/0015_3sum.py
@@ -31,3 +31,33 @@ class Solution:
                 else: 
                     l += 1
         return ans 
+    
+
+    def three_sum_revision_1(self, nums: List[int]) -> List[List[int]]:
+
+        nums.sort()
+        ans = [] 
+        for i,a in enumerate(nums):
+            req = 0 - a 
+            if a > 0: 
+                break 
+            l, r = i + 1, len(nums) - 1
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+
+            while l < r: 
+                total = nums[l] + nums[r]
+
+                if total == req: 
+                    ans.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1 
+                        continue
+                if total > req: 
+                    r -= 1 
+                else: 
+                    l += 1
+
+        return ans
