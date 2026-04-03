7e0f2690389948c8e1dcc643a49dc17de3360c84
romankarki <romnkrki@gmail.com>
2026-04-02 19:35:35 -0500

    best time to buy and sell stocks

---
+++ Company/Tesla/121_best_time_to_buy_stocks.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+
+    def max_profit(self, prices: List[int]) -> int: 
+        if len(prices) == 1:
+            return 0 
+        l, r = 0, 1
+        max_profit = 0
+        while l < r and r < len(prices):
+            profit = prices[r] - prices[l]
+            if profit < 0:
+                l = r 
+                r += 1 
+                continue
+            max_profit = max(profit, max_profit)
+            r += 1 
+
+        return max_profit
