0b3364c7bfb16b04f7897924ade0cf5ed6600ea0
romankarki <romnkrki@gmail.com>
2026-04-02 19:24:21 -0500

    letter combination of phone number revision 1

--- Company/Tesla/17_letter_combination_of_phone_number.py
+++ Company/Tesla/17_letter_combination_of_phone_number.py
@@ -32,3 +32,36 @@ class Solution:
 
         dfs(0, [])
         return res 
+    
+
+    def letter_combination_of_number_revision_1(self, digits: str) -> List[str]:
+        t = {
+            '1': [],
+            '2': ['a', 'b', 'c'],
+            '3': ['d','e','f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6' : ['m', 'n', 'o'],
+            '7': ['p','q','r','s'],
+            '8': ['t','u','v'],
+            '9': ['w','x','y','z']
+        }
+        res = []
+        def dfs(i, curr):
+            if len(curr) == len(digits):
+                word = "".join(curr)
+                res.append(word)
+                return 
+            if i >= len(digits):
+                return 
+            arr = t[digits[i]]
+            for each in arr:
+                curr.append(each)
+                dfs(i+1, curr)
+                curr.pop()
+
+
+        dfs(0, [])
+
+        return res 
+    
