245e032a9bf1ead122c1bc75bfa4dc7460729de1
romankarki <romnkrki@gmail.com>
2026-04-04 10:48:47 -0500

    reorganize string revision heapq version

--- Company/Tesla/767_reorganize_string.py
+++ Company/Tesla/767_reorganize_string.py
@@ -27,5 +27,45 @@ class Solution:
             if -cnt > 1:
                 return "" # impossible 
             result.append(ch)
-            
+
+        return "".join(result)
+    
+
+    def reorganize_string_revision_1(self, s: str) -> str:
+
+        '''
+            abcacbcacbc
+            a 3
+            b 3
+            c 5  -> c5, a3, b3 -> c4, a2, b3 -> c3, a2, b2 -> c2, a1, b2+ c1 a1 b1 -> c
+
+            ca + cb + ca + cb + ab + c => cacbcacbabc
+        
+        '''
+        
+        counts = Counter(s)
+        result = []
+        heap = [(-cnt, ch) for cnt, ch in counts.items()]
+        heapq.heapify(heap)
+
+        while len(heap) >= 2:
+            cnt1, ch1 = heapq.heappop(heap)
+            cnt2, ch2 = heapq.heappop(heap)
+            result.append(ch1)
+            result.append(ch2)
+
+            cnt1 = cnt1 + 1 
+            cnt2 = cnt2 + 1
+            if cnt1 < 0: 
+                heapq.heappush((cnt1, ch1))
+            if cnt2 < 0:
+                heapq.heappush((cnt2, ch2))
+
+        while heap:
+            cnt, ch = heap[0]
+            if -cnt > 1: 
+                return ""
+            result.append(ch)
+
+
         return "".join(result)
