33c98f4ea8b562f5cd0b6af121830695fcaa6aaa
romankarki <romnkrki@gmail.com>
2026-04-04 11:25:16 -0500

    happy number easy solution

---
+++ Company/Tesla/202_happy_number.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def isHappy(self,  n: int) -> bool:
+        n_set = set()
+        while n not in n_set:
+            sum_of_squares = 0 
+            n_set.add(n)
+            while n != 0 :
+                digit = n % 10 
+                sum_of_squares += digit ** 2
+                n =  n // 10
+        return False 
