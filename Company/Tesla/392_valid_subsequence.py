5acfbf100b09a102c72a7cbabfb465d43549d15c
romankarki <romnkrki@gmail.com>
2026-04-04 14:44:49 -0500

    revising is subsequence

--- Company/Tesla/392_valid_subsequence.py
+++ Company/Tesla/392_valid_subsequence.py
@@ -8,3 +8,13 @@ class Solution:
                 i += 1
             j += 1
         return i == len(s)
+    
+    def is_subsequence_revision1(self, s: str, t:str) -> bool:
+
+        i, j = 0, 0 
+
+        while i < len(s) and j < len(t):
+            if s[i] == s[j]:
+                i += 1
+            j += 1
+        return i == len(s)
